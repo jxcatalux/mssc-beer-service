8d120cc91b0c8317db921fda60cf10c1626878b7
jxcatalux <jxcanada@yahoo.ca>
2020-01-01 20:53:47 -0500

    Adding web Controller

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,16 +18,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID ID;
+
+    @Null
     private Integer version;
 
+    //@Null does not allow set the value from outside
+    @NotNull
     private OffsetDateTime createdDate;
+
+    @NotNull
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private  String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @Positive
+    @NotNull
     private  long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
